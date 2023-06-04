9c2fa206ce63ad6a249e75bb4926604153f72c94
DouglasDainese <douglas_dainese@hotmail.com>
2023-06-04 12:42:23 -0300

    fix: corrige erros lint

--- app/backend/src/services/tempCodeRunnerFile.ts
+++
@@ -1,6 +0,0 @@
-    const email = 'dougls@dsd.com';
-
-    const emailValidRegex = (/^[a-z0-9.]+@[a-z0-9]+\.[a-z]/i);
-    const checkEmail = email.match(emailValidRegex);
-
-    console.log(checkEmail);
